eb8cc15dfa1342d7bf9a6dd669e35cdf4777308c
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-05 17:13:04 -0200

    Alteração Controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
 
+  private
+ 
+  def sign_up_params
+    params.require(:user).permit(:foto, :nome_completo, :cidade, :estado,:email_usuario, :descricao, :email, :password, :password_confirmation)
+  end
+ 
+  def account_update_params
+    params.require(:user).permit(:foto, :nome_completo, :cidade, :estado,:email_usuario, :descricao, :email, :password, :password_confirmation, :current_password)
+  end
+
   # GET /resource/sign_up
   # def new
   #   super

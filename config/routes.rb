803d173fc9f568a138dc9027f71184152b525a9f
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-10 10:06:04 -0200

    Teste Search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 get 'users/show'
-resources :search
+resources 'search/'
 devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.

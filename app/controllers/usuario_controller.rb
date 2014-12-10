57cec65005f44be13aac6abac8510e9e2ac00668
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-10 21:23:29 -0200

    Modificando Search

--- app/controllers/usuario_controller.rb
+++ app/controllers/usuario_controller.rb
@@ -10,7 +10,7 @@ class UsuarioController < ApplicationController
   def pesquisar
   	search = params[:search]
   	if !search.eql?("")
-    	@users = User.where("nome_completo = #{search}").order(:nome_completo)
+    	@users = User.where("nome_completo = '#{search}'").order(:nome_completo)
   	else
     	@users = User.first(9)
   	end

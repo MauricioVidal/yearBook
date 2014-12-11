9115d00be9f75fdd64ca1e2f3734af4d986f8177
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-10 22:39:24 -0200

    Modificando Search

--- app/controllers/usuario_controller.rb
+++ app/controllers/usuario_controller.rb
@@ -14,7 +14,7 @@ class UsuarioController < ApplicationController
       sql <<"nome_completo LIKE '#{search}%' OR nome_completo LIKE '%#{search}%' OR "
       sql <<"nome_completo LIKE '#{search.capitalize}' OR nome_completo LIKE '%#{search.upcase}%"
       @users = User.where(sql).order(:nome_completo)
-      if @user.nil?
+      if @user.empty?
         redirect_to("/", :alert => "Nenhum registro encontrado!!")  
       end
   	else

aee58e487b3ac7d4f74bea985b90fd8eb6e2eff3
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 15:33:24 -0200

    Upload de Imagens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #after_save :save_photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :foto, :styles => { 
+    :medium => "300x300>", 
+    :thumb => "100x100>" }
 
   #validates :nome_completo, :cidade, :estado, :email_usuario, :descricao, :foto, :presence => true
   #attr_accessible :email, :password, :password_confirmation, :foto, :nome_completo, :cidade, :estado,:email_usuario, :descricao

d37f00bf463c08f8fd052e04f461553f8c9824c4
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 14:54:17 -0200

    Configurando Sign-Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  #PHOTO_STORE = File.join Rails.root.to_s, 'public', 'photos'
+  PHOTO_STORE = File.join Rails.root.to_s, 'public', 'photos'
 
   #after_save :save_photo
 
@@ -11,22 +11,22 @@ class User < ActiveRecord::Base
   #validates :nome_completo, :cidade, :estado, :email_usuario, :descricao, :foto, :presence => true
   #attr_accessible :email, :password, :password_confirmation, :foto, :nome_completo, :cidade, :estado,:email_usuario, :descricao
 
- # def load_image data
-  	# Record the filename
-  #   self.filename = data.original_filename
+ def load_image data
+  	#Record the filename
+   self.filename = data.original_filename
      # Store the data for later use
-   #  @photo_data = data
-  #end
+     @photo_data = data
+  end
 
-   #def save_photo
+   def save_photo
 
-	  #   if @photo_data
+	    if @photo_data
 	       # Write the data out to a file
-	   #    name = File.join PHOTO_STORE, self.filename
-	    #   File.open(name, 'wb') do |f|
-	     #  f.write(@photo_data.read)
-	      #	end
-	       #	@photo_data = nil
-    	#end
-	#end
+	     name = File.join PHOTO_STORE, self.filename
+	      File.open(name, 'wb') do |f|
+	       f.write(@photo_data.read)
+	      	end
+	       	@photo_data = nil
+    	end
+	end
 end

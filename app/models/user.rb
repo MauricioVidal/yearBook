70218b6c180df3421f2f6f26a445085258b0fae0
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 16:13:42 -0200

    Upload Imagem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :foto, :styles => { 
+  has_attached_file :avatar, :styles => { 
     :medium => "300x300>", 
     :thumb => "100x100>" }
 

26409a49547d83b5d8633aaafcaf4523e4c2ec06
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 14:45:27 -0200

    Configurando Sign-Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   #before_filter :configure_devise_params, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ 	protect_from_forgery with: :exception
+	before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  devise_parameter_sanitizer.for(:sign_up) << :foto <<:nome_completo << :cidade <<:estado <<:email_usuario <<:descricao << :email << :password << :password_confirmation
+  	protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| 
+      	u.permit(:foto ,:nome_completo , :cidade ,:estado ,:email_usuario ,:descricao , :email ,:password ,:password_confirmation) 
+      } end
+	end
 
 end

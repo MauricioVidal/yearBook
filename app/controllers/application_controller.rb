ed243e6995a1b11a5edd107ad8e19e30a53ac291
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 14:32:50 -0200

    Configurando Sign-Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  devise_parameter_sanitizer.for(:sign_up) << :foto <<:nome_completo << :cidade <<:estado <<:email_usuario <<:descricao << :email << :password << :password_confirmation
+
 end

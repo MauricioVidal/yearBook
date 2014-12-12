ec41981e415f93ce87e74b2ad844f3b0504499f1
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-12 14:47:39 -0200

    Configurando Sign-Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| 
       	u.permit(:foto ,:nome_completo , :cidade ,:estado ,:email_usuario ,:descricao , :email ,:password ,:password_confirmation) 
-      } end
+      }
 	end
 
 end

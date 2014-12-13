88e9c653b0203bf5c0ed2f057f0cf3738b80ec21
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-13 21:48:59 -0200

    Implementando Visitas

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { 
     :medium => "300x300>", 
     :thumb => "128x128>" }
+
+  has_many :visita, :foreign_key =>:user_visited_id, :dependence => :destroy
+
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates :nome_completo, :cidade, :estado, :email_usuario, :descricao, :avatar, :presence => true
   

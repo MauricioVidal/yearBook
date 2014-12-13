a2079576f4ce4ec807e44533590329bccd24d9ce
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-13 21:21:08 -0200

    Implementando Visitas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@user = current_user
-  	@visitas = Visita.select("Distinct user_visited_id ").where("current_user_id = '#{current_user.id}'").first(5);
+  	@visitas = Visita.select("user_visited_id ").where("current_user_id = '#{current_user.id}'").distinct.first(5);
   end
 end

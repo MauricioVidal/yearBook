b601c90ad55cfd42d230f5b56e20e877d41bdfea
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-13 18:56:33 -0200

    Implementando Visitas

--- app/controllers/usuario_controller.rb
+++ app/controllers/usuario_controller.rb
@@ -2,8 +2,10 @@ class UsuarioController < ApplicationController
   def show
   	if user_signed_in?
   		@user = User.find(params[:id])
-      @visita = Visita.new(current_user, @user)
-      @visita.save
+      if(!(current_user.eql?(@user))
+        @visita = Visita.new({current_user, @user})
+        @visita.save
+      end
   	else
   		redirect_to(new_user_session_path, :alert => "É necessario estar logado!!")
   	end

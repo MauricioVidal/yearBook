f01062446d532366cbb318b74de9deb66fe04857
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-13 00:09:22 -0200

    Redirecionamento de Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,13 @@ class ApplicationController < ActionController::Base
       }
 	end
 
-#  def after_sign_in_path_for(resource)
-     
- # end
+def after_sign_in_path_for(resource)
+    sign_in_url = url_for(:action => 'index', :controller => 'myHome')
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 
 end

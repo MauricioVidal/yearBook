3a9fe7865ff83f71ba1cd59a2cd95e6d33253914
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-14 20:18:18 -0200

    Alteraçoes Finais

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@user = current_user
-  	@visitas = Visita.where("current_user_id = #{@user.id}").distinct.pluck(:user_visited_id)
+  	@visitas = Visita.where("current_user_id = #{@user.id}").distinct.pluck(:user_visited_id).first(3)
   end
 end

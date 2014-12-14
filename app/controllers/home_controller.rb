1424113af3c1e1e8cd88b8582d9bf5d3ed74a4c0
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-14 20:29:39 -0200

    Alteraçoes Finais

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
+  	if user_signed_in?
   	@user = current_user
   	@visitas = Visita.where("current_user_id = #{@user.id}").distinct.pluck(:user_visited_id).first(3)
+  	else
+  		redirect_to(new_user_session_path, :alert => "É necessario estar logado!!")
+  	end
   end
 end

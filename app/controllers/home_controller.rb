dbc0feb7039c7154bb8e8f09f249e1bc8434cfb6
Mauricio Vidal <mauriciovidalxc@hotmail.com>
2014-12-13 22:08:01 -0200

    Implementando Visitas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@user = current_user
-  	@visitas = Visita.select("user.nome_completo").joins(:user).distinct
+  	@visitas = Visita.select("user.nome_completo").joins(:user).distinct.first(5)
   end
 end
